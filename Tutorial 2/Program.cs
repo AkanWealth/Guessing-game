d565d128f7581475c015b3e4402f4802e77fabb1
Akan Asanga <akanasanga@outlook.com>
2021-08-14 18:35:42 +0100

    Add project files.

---
+++ Tutorial 2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Tutorial_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string guessWord = "GUESS";
+            string guess = "";
+
+            int guessCount = 1;
+            int guessLimit = 4;
+            bool outOfGuesses = false;
+
+            while(guess != guessWord.ToLower() && !outOfGuesses)
+            {
+                if(guessCount < guessLimit)
+                {
+                    Console.Write("Enter guess: ");
+                    guess = Console.ReadLine();
+                    Console.WriteLine("You have used " +guessCount+" from your 3 guess (guess is 5 letters)");
+                    guessCount++;
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }
+            }
+            if (outOfGuesses)
+            {
+                Console.WriteLine("YOU LOSE !");
+            }
+            else
+            {
+            Console.WriteLine("YOU WIN !!!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
